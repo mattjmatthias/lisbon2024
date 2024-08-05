81e5607aaaaf1c08a3b717392988c6c1ebc857b8
Matt Matthias <mattjmatthias@hotmail.com>
2024-08-05 13:44:41 +0100

    Sending up start of header

---
+++ app/components/button-grid.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import React from 'react';
+
+type Button = {
+  id: string;
+  label: string;
+};
+
+type ButtonGridProps = {
+  buttons: Button[];
+};
+
+const ButtonGrid: React.FC<ButtonGridProps> = ({ buttons }) => {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {buttons.map((button, index) => (
+        <Link href={`#${button.id}`} key={index} className="">
+          <div className="block p-4 bg-black text-white text-center text-xl font-bold uppercase cursor-pointer hover:bg-pink-600">
+            <p className="">{button.label}</p>
+            <p className="text-xs font-normal">{button.t}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default ButtonGrid;
---
+++ app/components/place.tsx
@@ -0,0 +1,38 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+
+const Place: NextPage = () => {
+  return (
+    <section className="self-stretch flex flex-row items-start justify-start pt-0 px-0 box-border max-w-full text-left text-10xl text-white font-futura mq825:flex-col mq825:box-border">
+      <div className="flex-1 flex flex-row items-center justify-center p-[80px] mq825:p-[50px] text-black box-border gap-[118px] mq825:gap-[29px] mq1425:gap-[59px] max-w-full lg:flex-wrap mq825:flex-col mq825:box-border mq1425:box-border">
+        <div className="flex-1 flex flex-col items-start justify-start px-0 pb-0 box-border max-w-full lg:flex-1 mq825:min-w-full mq825:w-full">
+          <div className="self-stretch flex flex-col items-start justify-start gap-[24px]">
+            <div className="m-0 self-stretch relative text-inherit z-[1] mq450:text-3xl mq825:text-10xl">
+              <div className="flex">
+                <span className="pt-[7.1px] font-regular text-xl mr-2">at</span>
+                <a href="https://www.xandwhy.co.uk/clubspace/eighteen">
+                 
+                </a>
+              </div>
+            </div>
+            <div className="self-stretch relative  leading-[150%] text-3xl tracking-[-0.75px] font-small-text text-gainsboro-200 z-[1] mq450:text-lgi mq450:leading-[29px]">
+              Eighteen is a private bar and outdoor terrace on the 18th floor of the new 103 Colmore Row building in Birmingham City Centre
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 flex items-start justify-center mq825:w-full">
+          <a href="https://www.xandwhy.co.uk/clubspace/eighteen">
+            <img
+              className="flex-1 relative rounded-3xs max-w-full overflow-hidden object-cover z-[1] mq825:min-w-full"
+              loading="lazy"
+              alt=""
+              src="/eighteen4@2x.jpg"
+            />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Place;

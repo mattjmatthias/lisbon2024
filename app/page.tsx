118e378bda2f9106fb0cadee4e05f762ee7b2e8d
Matt Matthias <mattjmatthias@hotmail.com>
2024-08-05 14:06:39 +0100

    Sending up a reformat for larger screens

--- app/page.tsx
+++ app/page.tsx
@@ -16,22 +16,22 @@ export default function Home() {
   ];
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 gradient">
-      <div className="z-10 w-full max-w-5xl text-4xl text-white items-center justify-between font-medium lg:flex">
+    <main className="flex min-h-screen flex-col">      
       <div className="relative overflow-hidden">
-        <div className="absolute inset-0 h-[200px] max-h-[200px]">
+        <div className="absolute inset-0 w-full h-[200px] max-h-[200px]">
           <Image src={header} alt="Header image" fill={true} className="object-cover "/>          
         </div>
-        <div className="relative flex items-center justify-center text-center py-4 pr-1.5">
-          <h1 className="m-0 w-full text-[50px] relative text-right tracking-[-4px] font-bold font-inherit whitespace-pre-wrap inline-block max-w-full z-[3] leading-[38px] mix-blend-screen">
-            BECKY&apos;S 40TH
-            LISBON 2024
+        <div className="relative flex items-center justify-center text-center py-4 pr-1.5 md:p-8">
+          <h1 className="m-0 w-full text-[50px] md:text-[80px] relative text-right tracking-[-4px] font-bold font-inherit whitespace-pre-wrap inline-block max-w-full z-[3] md:leading-[60px] leading-[38px] mix-blend-screen">
+            <p>BECKY&apos;S 40TH</p>
+            <p>LISBON 2024</p>
           </h1>
         </div>
       </div>
 
       <ButtonGrid buttons={buttons} />
-      </div>
+      <div className="h-[200px] w-full gradient"/>
+      
     </main>
   );
 }
